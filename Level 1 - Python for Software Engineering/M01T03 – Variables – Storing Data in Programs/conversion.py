4a21327da5a4008206245f3f1d302690ddc82c1d
char-rea <charlotte.hill@rea-group.com>
2026-04-08 13:53:28 +0800

    Add conversion and details scripts for user input and data type demonstration

---
+++ Level 1 - Python for Software Engineering/M01T03 – Variables – Storing Data in Programs/conversion.py
@@ -0,0 +1,14 @@
+# Assigning a string "4" to the variable surprise_1
+surprise_1 = "4"
+# Assigning a floating-point number 4.5 to the variable surprise_2
+surprise_2 = 4.5
+# Assigning a string "four" to the variable surprise_3
+surprise_3 = "four"
+# Assigning the Boolean value True to the variable surprise_4
+surprise_4 = True
+
+# Use the print() and type() functions to check the data types.
+print(type(surprise_1))
+print(type(surprise_2))
+print(type(surprise_3))
+print(type(surprise_4))
---
+++ Level 1 - Python for Software Engineering/M01T03 – Variables – Storing Data in Programs/details.py
@@ -0,0 +1,12 @@
+# 1. Ask the user for their name
+# 2. Ask the user for their age
+# 3. Ask the user for their House number
+# 4. Ask the user for their Street name
+# Print all details from User
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+House_number = input("Enter your House number: ")
+Street_name = input("Enter your Street name: ")
+
+print(f"This is {name}, who is {age} years old and lives at house number {House_number} on {Street_name}.")
