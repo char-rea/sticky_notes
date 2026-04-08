c784175bb8060ea55c13c0395d0d8a90fc9e686d
char-rea <charlotte.hill@rea-group.com>
2026-04-08 11:12:27 +0800

    Add hello_world.py script to prompt user for name and age

---
+++ Level 1 - Python for Software Engineering/M01T02 – Your First Computer Program/hello_world.py
@@ -0,0 +1,7 @@
+# this code askt he user for name, age and prints name, age and "Hello, World!"
+# 
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print(name, age)
+print ("Hello, World!")
