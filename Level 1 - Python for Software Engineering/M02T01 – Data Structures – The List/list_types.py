aaafe1c54ea38b89f507108e32d2dfc6aa009dab
char-rea <charlotte.hill@rea-group.com>
2026-04-15 11:33:00 +0800

    Add list_types.py to store friends' names and ages with corresponding outputs

---
+++ Level 1 - Python for Software Engineering/M02T01 – Data Structures – The List/list_types.py
@@ -0,0 +1,13 @@
+#store the names of your friends in a list and print the first and last name in the list, as well as the number of friends in the list. Then, create a second list that stores the ages of your friends and print each friend's name along with their age.
+
+friends_names = ["Ross", "Rachel", "Joey"]
+
+print ("This is the name of your first friend in the list:", friends_names[0]) # print the first name in the list
+print ("This is the name of your last friend in the list:", friends_names[2]) # print the last name in the list
+print ("This is the number of friends in the list:", len(friends_names)) # print the number of names in the list
+
+friends_ages = [25, 30, 35]
+
+print (friends_names[0], "is aged", friends_ages[0]) # print the first age in the list
+print (friends_names[1], "is aged", friends_ages[1]) # print the middle age in the list
+print (friends_names[2], "is aged", friends_ages[2]) # print the last age in the list
