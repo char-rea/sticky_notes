1d7a6e608e604046ffd06ea4dda086bc62277437
Charlotte Hill <charlotte.hilll@rea-group.com>
2026-05-12 11:01:43 +0800

    Updated Task manager

--- Level 1 - Python for Software Engineering/M03T10 – Capstone Project – Task Manager/Code Files/task_manager.py
+++ Level 1 - Python for Software Engineering/M03T10 – Capstone Project – Task Manager/Code Files/task_manager.py
@@ -41,46 +41,30 @@ print("You have successfully logged in.")
 # This is the menu section of the program, where the user can select
 # one of the following options:
 while True:
-
-    if login_username == 'admin':
-        menu = input(
-            '''Select one of the following options:
+    menu = input(
+        '''Select one of the following options:
 r - register a user
 a - add task    
-va - view all tasks
-vm - view my tasks
-vc - view completed tasks
-del - delete tasks
-e - exit
-: '''
-        ).lower()
-    else:   
-        menu = input(
-        '''Select one of the following options:
-a - add task
 va - view all tasks
 vm - view my tasks
 e - exit
 : '''
     ).lower()
 
-
-# The following code block will allow an ADMIN user to register a new 
-# User by adding their username and password to the user.txt file.
-# The program will also check if the password and confirm password 
-# Match before adding the new user to the file.
-#admin only feature to register a new user
-    if menu == 'r' and login_username == 'admin':
+    # User by adding their username and password to the user.txt file.
+    # The program will also check if the password and confirm password 
+    # Match before adding the new user to the file.
+    if menu == 'r':
         new_username = input("Please enter a new username: ")
         new_password = input("Please enter a new password: ")
         confirm_password = input("Please confirm your password: ")
+
         if new_password == confirm_password:
             with open('user.txt', 'a', encoding='utf-8') as user_file:
                 user_file.write(f"\n{new_username}, {new_password}")
             print("New user registered successfully.\n")
         else:
-            print("Passwords do not match. Please try again.")
-
+            print("Passwords do not match. Please try again.\n")
 
 # The following code block will allow a user to add a new task 
 # To the task.txt File. The user will be prompted to enter the 
@@ -100,7 +84,6 @@ e - exit
         with open('tasks.txt', 'a', encoding='utf-8') as task_file:
             task_file.write(f"\n{username}, {title}, {description}, {due_date}, {current_date}, No")
         print("Task added successfully.\n")
-
 
 # The following code block will read the task.txt file and 
 # Print all the tasks to the console in a formatted way.
@@ -123,8 +106,7 @@ e - exit
                 print(f"Due Date: \t{due_date}")
                 print(f"Task Complete? \t{completed}")
                 print(f"Task Description:\n{description}")
-                print("-" * 50)
-
+            print("-" * 50)
 
 # The following code block will read the task.txt file and
 # Print only tasks that are assigned to the user who is currently logged in.
@@ -151,56 +133,10 @@ e - exit
                     print(f"Task Description:\n{description}")
                     print("-" * 50)
 
-# Admin only - view completed tasks                 
-# The following code block will read the task.txt file and
-# Print only tasks that are marked as complete to the console in a formatted way.
-    elif menu == 'vc' and login_username == 'admin':    
-        with open('tasks.txt', 'r', encoding='utf-8') as task_file:
-            for line in task_file:
-                task_details = line.strip().split(', ')
-
-                task_username = task_details[0]
-                title = task_details[1]
-                description = task_details[2]
-                assigned_date = task_details[3]
-                due_date = task_details[4]
-                completed = task_details[5]
-
-                if completed.lower() == 'yes':
-                    print("-" * 50)
-                    print(f"Task: \t\t{title}")
-                    print(f"Assigned to: \t{task_username}")
-                    print(f"Date Assigned: \t{assigned_date}")
-                    print(f"Due Date: \t{due_date}")
-                    print(f"Task Complete? \t{completed}")
-                    print(f"Task Description:\n{description}")
-                    print("-" * 50)
-
-# Admin only - delete tasks
-# The following code block will allow an admin user to 
-# Delete a task from the task.txt file
-
-    elif menu == 'del' and login_username == 'admin':
-        task_to_delete = input("Enter the title of the task you want to delete: ")
-
-        with open('tasks.txt', 'r', encoding='utf-8') as task_file:
-            tasks = task_file.readlines()
-
-        with open('tasks.txt', 'w', encoding='utf-8') as task_file:
-            for line in tasks:
-                if task_to_delete not in line:
-                    task_file.write(line)
-
-        print(f"Task '{task_to_delete}' has been deleted successfully.\n")
-        else:
-            print("Invalid task title. Please try again.\n")
-            
-
 # Allow the user to exit the program by selecting the 'e' option from the menu.
     elif menu == 'e':
         print('Goodbye, Thanks for using the Task Manager!!!')
         exit()
-
 
 # Error message for invalid menu option
     else:
