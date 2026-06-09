de8b2dcca24bedbc551f0fe97e830c8464b03b6b
Charlotte Hill <charlotte.hilll@rea-group.com>
2026-06-09 12:35:31 +0800

    remove old sticky notes

--- Level 2 - Introduction to Software Engineering/M06T05 – Django – Sticky Notes Application Part 2/sticky_notes/sticky_notes/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-# Define URL patterns for the entire project
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('notes.urls')),
-]
